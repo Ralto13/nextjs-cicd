f84c63ff4395d81354255a3e0ec106cbb59aa6fc
Ralto13 <doorumi13@gmail.com>
2025-02-03 18:35:34 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp;
